e56adffc732a1ab448f9d04e8a0122de8133c943
abhaysirohii <abhaysirohi321@gmail.com>
2026-07-01 12:26:32 +0530

    Time: 25 ms (12.4%), Space: 81.2 MB (77.27%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,15 +1,17 @@
+import java.util.Arrays;
+
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        HashSet<Integer> set= new HashSet<>();
-        for(int i=0;i<nums.length;i++){
-            if(set.contains(nums[i])){
+        // Step 1: Sort the array. Duplicates will now be adjacent.
+        Arrays.sort(nums);
+        
+        // Step 2: Check neighboring elements
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
                 return true;
             }
-            else{
-                set.add(nums[i]);
-            }
         }
-        return false;
         
+        return false;
     }
 }

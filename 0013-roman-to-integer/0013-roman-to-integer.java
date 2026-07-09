8e583e0ca819c0a762d32e25b08395106c87b6d7
abhaysirohii <abhaysirohi321@gmail.com>
2026-07-09 20:06:42 +0530

    Time: 5 ms (33.4%), Space: 46.4 MB (75.4%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -6,10 +6,7 @@ class Solution {
         int result=0;
         for(int i=0;i<s.length();i++){
             int curr = map.get(s.charAt(i));
-            int next=0;
-            if(i+1<s.length()){
-                next=map.get(s.charAt(i+1));
-            }
+            int next = (i + 1 < s.length()) ? map.get(s.charAt(i + 1)) : 0;
             if(next>curr){
                 result-=curr;
             }

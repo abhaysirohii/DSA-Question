95258856c41337fb0695b9ebc20b3a3abe5b6298
Abhay Sirohi <abhaysirohi321@gmail.com>
2026-07-12 14:53:23 +0530

    Time: 5 ms (99.8%), Space: 45.9 MB (31.67%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,37 +1,51 @@
+import java.util.*;
+
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        List<List<Integer>> result= new ArrayList<>();
-        int n= nums.length-1;
+        List<List<Integer>> result = new ArrayList<>();
+        if (nums == null || nums.length < 4) return result;
+        
+        int n = nums.length;
         Arrays.sort(nums);
-        for(int i =0 ;i<n-2;i++){
+        
+        for (int i = 0; i < n - 3; i++) {
             if (i > 0 && nums[i] == nums[i - 1]) continue;
-            for (int j = i + 1; j < n - 1; j++) {
+            
+            // Optimization 1: Smallest possible sum from this point is too big
+            if ((long) nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3] > target) break;
+            // Optimization 2: Largest possible sum from this point is too small
+            if ((long) nums[i] + nums[n - 3] + nums[n - 2] + nums[n - 1] < target) continue;
+            
+            for (int j = i + 1; j < n - 2; j++) {
                 if (j > i + 1 && nums[j] == nums[j - 1]) continue;
-            int left=j+1;
-            int right=n;
-                while(left<right){
-                    long sum=(long) nums[i] + nums[j] +nums[left] + nums[right];
-                    if(sum==target){
-                        result.add(Arrays.asList(nums[i],nums[j],nums[left],nums[right]));
+                
+                // Optimization 3: Smallest possible sum from this 'j' is too big
+                if ((long) nums[i] + nums[j] + nums[j + 1] + nums[j + 2] > target) break;
+                // Optimization 4: Largest possible sum from this 'j' is too small
+                if ((long) nums[i] + nums[j] + nums[n - 2] + nums[n - 1] < target) continue;
+                
+                int left = j + 1;
+                int right = n - 1;
+                
+                while (left < right) {
+                    long sum = (long) nums[i] + nums[j] + nums[left] + nums[right];
+                    
+                    if (sum == target) {
+                        result.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
 
-                        while (left < right && nums[left] == nums[left + 1]) 
-                        left++;
-
+                        while (left < right && nums[left] == nums[left + 1]) left++;
                         while (left < right && nums[right] == nums[right - 1]) right--;
                         
-                       left++;
-                       right--;
-                    }
-                    else if(sum<target){
-                    left++;
-                    }
-                    else{
-                    right--;
+                        left++;
+                        right--;
+                    } else if (sum < target) {
+                        left++;
+                    } else {
+                        right--;
                     }
                 }
             }
         }
         return result;
-        
     }
 }

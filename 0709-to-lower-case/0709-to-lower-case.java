2da168ab432faa977c0aaefa60109d88fbf46d65
Abhay Sirohi <abhaysirohi321@gmail.com>
2026-08-07 22:47:06 +0530

    Time: 0 ms (100%), Space: 43 MB (51.56%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.java
+++ 0709-to-lower-case/0709-to-lower-case.java
@@ -1,14 +1,5 @@
 class Solution {
     public String toLowerCase(String s) {
-        String result="";
-        for(int i=0;i<s.length();i++){
-            char curr=s.charAt(i);
-            if(curr>= 65 && curr<=90){
-                result+=(char)(curr+32);
-            }else{
-                result+=curr;
-            }
-        }
-        return result;
+        return s.toLowerCase();
     }
 }
